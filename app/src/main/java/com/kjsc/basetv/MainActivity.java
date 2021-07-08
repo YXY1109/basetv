cca1f42daffce71bfd64742d2d97e99216287026
522917595@qq.com <chengjie1004>
2021-07-08 10:34:28 +0800

    优化基础代码

--- app/src/main/java/com/kjsc/basetv/MainActivity.java
+++ app/src/main/java/com/kjsc/basetv/MainActivity.java
@@ -3,12 +3,17 @@ package com.kjsc.basetv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.kjsc.basetv.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
     }
 }
